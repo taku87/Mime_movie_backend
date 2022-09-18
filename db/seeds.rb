d8d8b1226c82927ab39ed4220dd4f7176b24585b
hanamura takuya <takuya0hanamura@gmail.com>
2022-09-18 18:46:34 +0900

    Add: サンプルデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "テストユーザー１", email: "test1@example.com")
+User.create(name: "テストユーザー２", email: "test2@example.com")
+User.create(name: "テストユーザー３", email: "test3@example.com")
+
+ContentVideo.create(number: "100", title: "コンテンツ動画１")
+ContentVideo.create(number: "200", title: "コンテンツ動画２")
+ContentVideo.create(number: "300", title: "コンテンツ動画３")
+
+Tag.create(title: "テストタグ１")
+Tag.create(title: "テストタグ２")
+Tag.create(title: "テストタグ３")
+
+#UserVideo.create(user_id: 1, content_video_id: 1, number: "1", content_number: "100", video_url: "uploads/user_video/video_url/1")
+#UserVideo.create(user_id: 1, content_video_id: 2, number: "2", content_number: "200", video_url: "uploads/user_video/video_url/2")
+#UserVideo.create(user_id: 1, content_video_id: 3, number: "3", content_number: "300", video_url: "uploads/user_video/video_url/3")
