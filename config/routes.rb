0a01444f4fb63e6275812b3f4b68cd70aeeb2823
hanamura <takuya0hanamura@gmail.com>
2022-09-19 11:55:05 +0900

    Add:ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Rails.application.routes.draw do
-  
-  resources :users
-  root  'user_videos#index'
-  resources :user_videos
+
+  namespace :admin, format: 'json' do
+    resources :content_videos do
+      resources :tags
+      resources :content_video_likes, only: [:create, :destroy]
+      resources :content_video_loves, only: [:create, :destroy]
+      resources :content_video_comments, only: [:create, :edit, :update, :destroy]
+    end
+  end
+
+  namespace :api, format: 'json' do
+    namespace :v1 do
+      namespace :user do
+        resources :user_videos, only: [:create, :destroy]
+        resources :completed_video_likes, only: [:create, :destroy]
+        resources :completed_video_loves, only: [:create, :destroy]
+        resources :completed_video_comments, only: [:create, :edit, :update, :destroy]
+      end
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+#only: [:index, :show, :new, :edit, :create, :update, :destroy]
