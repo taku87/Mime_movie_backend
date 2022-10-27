2c2d563d88a1235b6b1f2419b216e8ce014da42c
hana <100251756+taku87@users.noreply.github.com>
2022-10-27 12:53:09 +0900

    Add:AWSの設定変更

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,4 @@
 Aws.config.update({
     region: 'ap-northeast-1',
-    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
     })
-    s3 = Aws::S3::Client.new(signature_version: 'v4')
-    Signer = Aws::S3::Presigner.new(client: s3)
