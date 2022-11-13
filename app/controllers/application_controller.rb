6e073baac3153e263b63c223129b2a1baf280395
hana <100251756+taku87@users.noreply.github.com>
2022-11-14 06:50:39 +0900

    Add:CSRF対策用の設定追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  include ActionController::RequestForgeryProtection
   protect_from_forgery with: :null_session
+
+  def set_csrf_token_header
+    response.set_header('X-CSRF-Token', form_authenticity_token)
+  end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Per-Page', 'Total', 'Link']
+      expose: ['Per-Page', 'Total', 'Link', 'X-CSRF-Token']
   end
 end
