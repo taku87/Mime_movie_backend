d91920cfbb0e11dd71fb3b9dd151ab0510c9eda9
hana <100251756+taku87@users.noreply.github.com>
2022-12-04 14:33:37 +0900

    Fix:corsの設定変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'http://localhost:3001', 'https://mime-movie.com', 'https://api.mime-movie.com']
+    origins 'http://localhost:3000', 'http://localhost:3001', 'https://mime-movie.com', 'https://api.mime-movie.com'
 
     resource "*",
       headers: :any,
