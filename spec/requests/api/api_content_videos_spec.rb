d33edb856432fa24451a78e22ac2b8d5c0874919
hana <100251756+taku87@users.noreply.github.com>
2022-12-25 00:23:51 +0900

    Add:管理者権限のコンテンツ動画管理機能のAPIテスト作成

--- spec/requests/api/api_content_videos_spec.rb
+++ spec/requests/api/api_content_videos_spec.rb
@@ -23,6 +23,58 @@ RSpec.describe "コンテンツ動画管理機能", type: :request do
       expect(json["data"]["attributes"]['number']).to eq(content_video.number)
     end
   end
+
+  describe "管理者機能" do
+    let!(:current_user) { create :user, role: 1}
+
+    before do
+      # authorize_requestメソッドが呼ばれたらlet!(:current_user)を返す。
+      # SecuredControllerのcurrent_userメソッドが呼ばれたらlet!(:current_user)を返す。
+      authorization_stub
+    end
+
+    create_params = {
+      number: 100,
+      title: "title",
+      description: "description",
+      thumbnail: "thumbnail",
+      youtube_url: "youtube_url",
+      role: 0,
+    }
+
+    describe "新規コンテンツ動画登録機能" do
+      it '新規のコンテンツ動画が登録されること' do
+        expect { post '/api/v1/admin/content_videos', params: create_params }.to change(ContentVideo, :count).by(+1)
+        expect(response.status).to eq(200)
+        created_content_video = ContentVideo.find_by(number: 100)
+        expect(created_content_video.title).to eq("title")
+      end
+    end
+
+    describe "コンテンツ動画更新機能" do
+      let!(:content_video) { create :content_video,
+                                      number: 100,
+                                      title: "title",
+                                      description: "description",
+                                      thumbnail: "thumbnail",
+                                      youtube_url: "youtube_url"
+                                    }
+      it 'コンテンツ動画が更新されること' do
+        put "/api/v1/admin/content_videos/#{content_video.id}", params: {title: "another_title"}
+        expect(response.status).to eq(200)
+        created_content_video = ContentVideo.find_by(number: 100)
+        expect(created_content_video.title).to eq("another_title")
+      end
+    end
+
+    describe "コンテンツ動画削除機能" do
+      let!(:content_video) { create :content_video, number: 100}
+      it 'コンテンツ動画が削除されること' do
+        expect { delete "/api/v1/admin/content_videos/#{content_video.id}"}.to change(ContentVideo, :count).by(-1)
+        expect(response.status).to eq(200)
+      end
+    end
+  end
 end
 
 
